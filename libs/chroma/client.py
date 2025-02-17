502487fa0f3642421c18c6989abe8d63504b3b3e
Marco Caimi <mcaimi@redhat.com>
2025-02-17 16:53:45 +0100

    Introduce LlamaIndex to wrap ChromaDB remote and local instances

--- libs/chroma/client.py
+++ libs/chroma/client.py
@@ -8,8 +8,12 @@ from chromadb import PersistentClient, Collection
 from libs.loaders.dataloader import prepare_corpus
 from libs.loaders.formats import loaders
 from libs.splitters.splitters import split_text_documents_nltk as splitter
+from libs.loaders.dirloader import dirLoader, loadDocuments
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import VectorStoreIndex, StorageContext
 
 
+# ChromaDB client for a local session, langchain version
 class ChromaClient(object):
     def __init__(self, persistence_directory: str = ".",
                  collection: str = "default",
@@ -60,3 +64,44 @@ class ChromaClient(object):
         # cleanup
         del self.documents
 
+
+# llamaindex wrapper for a local chromadb instance
+class LlamaIndexChroma(object):
+    def __init__(self, persistence_directory: str = ".",
+                 collection: str = "default",
+                 collection_similarity: str = "l2",
+                 embedding_function: Callable = None):
+        self.persistence_dir: str = persistence_directory
+        self.collection_name = collection
+        self.collection_similarity = collection_similarity
+        if embedding_function is None:
+            raise Exception("ChromaClient: embedding_function cannot be None, you must specify and embedding function")
+        else:
+            self._embed_function: Callable = embedding_function
+
+        # instantiate client and adapter
+        self.chroma_client: PersistentClient = PersistentClient(path=self.persistence_dir)
+        self._collection = self.chroma_client.get_or_create_collection(self.collection_name,
+                                                                       metadata={"hnsw:space": self.collection_similarity})
+        self._vector_store: ChromaVectorStore = ChromaVectorStore(chroma_collection=self._collection)
+        self._storage_context: StorageContext = StorageContext.from_defaults(vector_store=self._vector_store)
+
+    def Collection(self) -> Collection:
+        return self._collection
+
+    def GenerateEmbeddings(self, training_data_path: str = ".",
+                           pattern: dict = [".txt"],
+                           show_progress: bool = True):
+        # load custom knowledge data and tokenize it
+        data_loader = dirLoader(training_data_path, extensions=pattern)
+        knowledge_body = loadDocuments(data_loader)
+        print(f"Loaded {len(knowledge_body)} Documents...")
+
+        if len(knowledge_body) > 0:
+            self._vector_index = VectorStoreIndex.from_documents(knowledge_body,
+                                                                 storage_context=self._storage_context,
+                                                                 embed_model=self._embed_function,
+                                                                 show_progress=show_progress)
+
+    def __str__(self) -> str:
+        return f"ChromaDB Client: {self._client.database} - Collection: {self._collection}"
--- libs/chroma/remote_client.py
+++ libs/chroma/remote_client.py
@@ -9,8 +9,12 @@ from libs.loaders.dataloader import prepare_corpus
 from libs.loaders.formats import loaders
 from libs.splitters.splitters import split_text_documents_nltk as splitter
 from libs.vectorstore.remote import chroma_client
+from libs.loaders.dirloader import dirLoader, loadDocuments
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import VectorStoreIndex, StorageContext
 
 
+# plain langchain connector for a remote chromadb instance
 class RemoteChromaClient(object):
     def __init__(self, host: str = "localhost",
                  port: int = 8080,
@@ -44,8 +48,8 @@ class RemoteChromaClient(object):
                            multithread: bool = False):
         # load custom knowledge data and tokenize it
         knowledge_body = loaders[data_type](path=training_data_path,
-                                       pattern=pattern,
-                                       multithread=multithread)
+                                            pattern=pattern,
+                                            multithread=multithread)
         print(f"Loaded {len(knowledge_body)} Documents...")
 
         # prepare knowledge corpus
@@ -60,6 +64,52 @@ class RemoteChromaClient(object):
         if len(tokenized_docs) > 0:
             for doc in tqdm(tokenized_docs, ascii=True, desc="Ingesting..."):
                 self.Adapter().add_documents(ids=[str(uuid.uuid1())], documents=[doc])
+
+    def __str__(self) -> str:
+        return f"ChromaDB Client: {self._client.database} - Collection: {self._collection}"
+
+
+# llamaindex wrapper for a remote chromadb instance
+class LlamaIndexChromaRemote(object):
+    def __init__(self, host: str = "localhost",
+                 port: int = 8080,
+                 collection: str = "default",
+                 collection_similarity: str = "l2",
+                 embedding_function: Callable = None):
+        self._client: HttpClient = chroma_client(host=host, port=port)
+        if embedding_function is None:
+            raise Exception("RemoteChromaClient: embedding_function cannot be None: you must specify an embedding function")
+        else:
+            self._embed_function = embedding_function
+            self._collection: Collection = self._client.get_or_create_collection(collection, metadata={"hnsw:space": collection_similarity})
+            self._vector_store: ChromaVectorStore = ChromaVectorStore(chroma_collection=self._collection)
+            self._storage_context: StorageContext = StorageContext.from_defaults(vector_store=self._vector_store)
+
+    def Client(self) -> HttpClient:
+        return self._client
+
+    def Adapter(self) -> ChromaVectorStore:
+        return self._vector_store
+
+    def Collection(self) -> Collection:
+        return self._collection
+
+    def Heartbeat(self) -> int:
+        return self._client.heartbeat()
+
+    def GenerateEmbeddings(self, training_data_path: str = ".",
+                           pattern: dict = [".txt"],
+                           show_progress: bool = True):
+        # load custom knowledge data and tokenize it
+        data_loader = dirLoader(training_data_path, extensions=pattern)
+        knowledge_body = loadDocuments(data_loader)
+        print(f"Loaded {len(knowledge_body)} Documents...")
+
+        if len(knowledge_body) > 0:
+            self._vector_index = VectorStoreIndex.from_documents(knowledge_body,
+                                                                 storage_context=self._storage_context,
+                                                                 embed_model=self._embed_function,
+                                                                 show_progress=show_progress)
 
     def __str__(self) -> str:
         return f"ChromaDB Client: {self._client.database} - Collection: {self._collection}"
