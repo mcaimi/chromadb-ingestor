5fc574cf1e652e603f8b681cc9725f62f47a6053
Marco Caimi <mcaimi@redhat.com>
2025-05-07 12:01:04 +0200

    Updated chromadb client code

--- libs/chroma/client.py
+++ libs/chroma/client.py
@@ -3,6 +3,9 @@
 from typing import Callable
 from chromadb import PersistentClient, Collection
 from libs.loaders.dirloader import dirLoader, loadDocuments
+from libs.splitters.semantic_splitter import semanticSplitterPipeline
+from libs.utils.tools import splitList
+from libs.loaders.dataloader import prepare_corpus
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import VectorStoreIndex, StorageContext
 
@@ -32,18 +35,27 @@ class LlamaIndexChroma(object):
 
     def GenerateEmbeddings(self, training_data_path: str = ".",
                            pattern: dict = [".txt"],
-                           show_progress: bool = True):
+                           show_progress: bool = True, batches: int = 1):
         # load custom knowledge data and tokenize it
         data_loader = dirLoader(training_data_path, extensions=pattern)
-        knowledge_body = loadDocuments(data_loader)
+        knowledge_body = prepare_corpus(loadDocuments(data_loader))
         print(f"Loaded {len(knowledge_body)} Documents...")
+        splitterPipeline = semanticSplitterPipeline(documents=knowledge_body,
+                                                    embedder=self._embed_function)
+        # run node splitter
+        print("Splitting documents in semantically similar chunks...")
+        nodes_list = splitterPipeline.run(documents=knowledge_body)
+        print(f"Produced {len(nodes_list)} semantically split nodes")
 
-        if len(knowledge_body) > 0:
-            for doc in knowledge_body:
-                self._vector_index = VectorStoreIndex.from_documents([doc],
-                                                                     storage_context=self._storage_context,
-                                                                     embed_model=self._embed_function,
-                                                                     show_progress=show_progress)
+        print(f"Preparing {batches} node batches...")
+        nodes: list = splitList(nodes_list, batches)
+
+        if len(nodes) > 0:
+            for batch_of_nodes in nodes:
+                self._vector_index = VectorStoreIndex(batch_of_nodes,
+                                                      storage_context=self._storage_context,
+                                                      embed_model=self._embed_function,
+                                                      show_progress=show_progress)
 
     def __str__(self) -> str:
         return f"ChromaDB Client: {self._client.database} - Collection: {self._collection}"
