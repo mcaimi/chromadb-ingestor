2768ebe7c62ada550f7f8921188299ae33433b34
Marco Caimi <mcaimi@redhat.com>
2025-05-07 12:01:18 +0200

    Updated dataloader code

--- libs/loaders/dataloader.py
+++ libs/loaders/dataloader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 try:
-    from langchain_core.documents import Document
+    from llama_index.core import Document
     from nltk import word_tokenize, sent_tokenize, download
     from prettytable import PrettyTable
     from tqdm import tqdm
@@ -16,12 +16,13 @@ def prepare_corpus(raw_loader: list) -> list:
     # collect statistics in relation to the data corpus
     corpus = []
     for doc in raw_loader:
-        document_tokens = word_tokenize(doc.page_content)
-        document_sentences = sent_tokenize(doc.page_content)
+        page_content = doc.get_content()
+        document_tokens = word_tokenize(page_content)
+        document_sentences = sent_tokenize(page_content)
         if (len(document_tokens) != 0) and (len(document_sentences) != 0):
             corpus.append({"metadata": doc.metadata,
                            "raw_sentences": document_sentences,
-                           "page_content": doc.page_content,
+                           "page_content": page_content,
                            "sentence_count": len(document_sentences),
                            "wordcount": len(document_tokens),
                            "vocabulary": set(document_tokens),
@@ -29,9 +30,9 @@ def prepare_corpus(raw_loader: list) -> list:
 
     # display corpus
     data_table = PrettyTable()
-    data_table.field_names = ["Dataset", "Word Count", "Sentence Count", "Vocabulary", "Lexical Richness"]
+    data_table.field_names = ["Dataset", "Word Count", "Sentence Count", "Vocabulary", "Lexical Richness", "File Type"]
     for dataset in corpus:
-        data_table.add_row([dataset.get("metadata").get("source"), dataset.get("wordcount"), dataset.get("sentence_count"), len(dataset.get("vocabulary")), dataset.get("lexical_richness")])
+        data_table.add_row([dataset.get("metadata").get("file_name"), dataset.get("wordcount"), dataset.get("sentence_count"), len(dataset.get("vocabulary")), dataset.get("lexical_richness"), dataset.get("metadata").get("file_type")])
 
     # display dataset statistics
     print(data_table)
@@ -40,5 +41,5 @@ def prepare_corpus(raw_loader: list) -> list:
     prepared_data = []
     for doc in tqdm(corpus, ascii=True, desc="Preparing..."):
         doc_metadata = doc.get("metadata")
-        prepared_data.append(Document(metadata=doc_metadata, page_content=doc.get("page_content")))
+        prepared_data.append(Document(metadata=doc_metadata, text=doc.get("page_content")))
     return prepared_data
